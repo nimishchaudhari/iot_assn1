ad8b6e46c6edaf9270aad7b575e6698d5371a3fa
21912044 <21912044@etu-web2.ut-capitole.fr>
2022-10-17 22:03:22 +0200

    commit on Monday 22h

--- components/js/ngparcelhistory.js
+++ components/js/ngparcelhistory.js
@@ -3,32 +3,88 @@ parcelApp = angular.module('ngParcelApp', []);
 parcelApp.controller('ParcelHistoryController',  function($scope, $http) {
 	
 	let URL_PARCEL_HIST = "http://localhost:3007/parcel_hist";
+	// let URL_PARCEL_HIST = "http://http://localhost:3007/enterparcelid";
 	// let URL_PARCEL_HIST = "http://etu-web2.ut-capitole.fr:3007/parcel_hist";
-	let URL_PARCEL_FINDONE = "http://localhost:3007/oneparcel_hist?";
-	// let URL_INSERT_LOC = "http://etu-web2.ut-capitole.fr:3007/insLocation?";
-	$scope.parcels = [];
+	let URL_PARCEL_FINDONE = "http://localhost:3007/show_parcel_info?";
+	let URL_PARCEL_ADDNEWOP = "http://localhost:3007/addNewOperation?"
+	let URL_PARCEL_DELETE = "http://localhost:3007/deleteRecord?"	
+	let URL_POPULATE_ADD = "http://localhost:3007/populateAddNew"
+	
+	
 	$http.get(URL_PARCEL_HIST).then(function(response) {
+	$scope.showHistory = false
 	
-    console.log(response)
-    console.log(response.test)
-	$scope.parcels =  response.data;
-	
+    $scope.parcels =  response.data;
+	// $scope.parcels.push(response.data)
+	$scope.tablebutton = 0
     });
 
-	
-	$scope.getparcels = function() {
+	$http.get(URL_POPULATE_ADD).then(function(response)
+	{
+		$scope._stuff = response.data
+		$scope.ops = ["boarding","transfer","delivering"]
 
-		let parcelinfo ={}
+
+	})
+	parcel_id = 0
+	$scope.getparcels = function() { // Gets parcels based on Parcel ID entered 
+
 		$http.get(URL_PARCEL_FINDONE + `parcelId=${$scope.parcelId}`)
 			.then(function (response) {
-				newRawdata = response.data;
-				
-				parcelinfo = newRawdata;
-		
 
-				$scope.parcels.push(parcelinfo);
+
+				$scope.parceloperations = response.data
+				// $scope.tablebutton = 1
+				showHistory =  true
+
+
+
+				// $scope.push($scope.showHistory)
+		
 			});
+
 	}
 	
-});
+	$scope.populateAddNew = function() {
+		$http.get(URL_POPULATE_ADD)
+		.then(function(response){
+			$scope.rows = response.data
+			// $scope.locId = response.data.locId
+			$scope.ops = response.data.ops 
 
+		})
+
+	}
+
+
+
+	$scope.parcel_add_operation = function() {
+		//parcelId=200
+
+		$http.get(URL_PARCEL_ADDNEWOP + `parcelId=${$scope.parcelId}&operation=${$scope.p_operation}&locId=${$scope.p_locId}&date=${$scope.p_date}`)
+		.then(function (response) {
+			newRawdata = response.data;
+		print("Add operation works")
+			$scope.singleparcel = newRawdata;
+	
+			});
+
+	}
+
+
+	//deleteparcel
+	$scope.deleteparcel = function(parc) {
+		
+			let confirmdeletemsg = window.confirm(" Are you sure you want to delete it? ")
+			if(confirmdeletemsg == true){
+				// alert(URL_PARCEL_DELETE + parc.parcelId +"&"+ parc.operation)
+				$http.get(URL_PARCEL_DELETE + `parcelId=${parc.parcelId}&operation=${parc.operation}`)
+				.then(function (response) {
+
+					$scope.singleparcel = response.data
+				
+				})
+				}
+}
+
+})
--- components/routes.js
+++ components/routes.js
@@ -3,21 +3,25 @@ const { getSystemErrorMap } = require("util");
 
 module.exports = (app, db) => {
 
-app.get('/parcelhist', function(req, res) { //Sending HTML
+	app.get('/enterparcelid', function(req, res) { //Sending HTML
      
-	// send the angular app
-    res.setHeader('Content-Type', 'text/html');
-    res.sendFile( __dirname + '/views' + '/nghistory.html');
-});
+		// send the angular app
+		res.setHeader('Content-Type', 'text/html');
+		res.sendFile( __dirname + '/views' + '/selectparcel.html');
+	});
+
+	
+// app.get('/parcelhistoryview', function(req, res) { //Sending HTML
+     
+// 	// send the angular app
+//     res.setHeader('Content-Type', 'text/html');
+//     res.sendFile( __dirname + '/views' + '/_ngHistory.html');
+// });
 
 
 
 app.get('/oneparcel_hist', function(req, res) {
-         
-	// History of a parcel html
-	// res.setHeader('Content-Type', 'text/html');
-	// res.sendFile( __dirname + '/views' + '/nghistory.html');
-	
+    
 	let parcelId = req.query.parcelId
 	console.log(req.query.parcelId)
 	let sql = 'SELECT Located.parcelId, Located.locId, Located.date, Located.operation FROM Located WHERE Located.parcelId = ?';
@@ -37,17 +41,31 @@ app.get('/oneparcel_hist', function(req, res) {
 });
 
 
+app.get('/show_parcel_info', function(req, res) {
+// Gets parcel info based on parcelId
+// need to get Operation, Location (from Location table), 
+	let sql = 'SELECT Located.parcelId, Located.date, Located.operation, Located.locId, Locations.city FROM Located, Locations WHERE Located.parcelId = ? AND Located.locId = Locations.locID';
+	
+	let values = [req.query.parcelId]
+	let postProcessSQL =   function (err, result) {
+		if (err) throw err;
+		console.log("In the new show_parcel info route")
+		// res.setHeader('Content-Type', 'text/html');
+    	// res.sendFile( __dirname + '/views' + '/ngHistory.html');
+		res.json(result);
+		
+	};
+
+	db.query(sql,values ,postProcessSQL);
+
+	
+});
 
 
 app.get('/parcel_hist', function(req, res) {
          
 	// History of all parcels html
-	// res.setHeader('Content-Type', 'text/html');
-	// res.sendFile( __dirname + '/views' + '/nghistory.html');
-	
-	// let parcelId = req.query.parcelId
-
-	let sql = 'SELECT locId, date, time, operation FROM Located' // WHERE parcelId = ?';
+	let sql = 'SELECT parcelId, locId, date, time, operation FROM Located' // WHERE parcelId = ?';
 	// let values = req.query
 	let values = []
 	console.log("Im running the query")
@@ -64,7 +82,86 @@ app.get('/parcel_hist', function(req, res) {
 	db.query(sql,values ,postProcessSQL);
 
 });
+app.get('/insertedNewRecord', function(req, res) {
 
+	res.setHeader('Content-Type', 'application/json');
+    res.end(res);
+
+})
+
+
+app.get('/deleteRecord', function(req, res) {
+	let parcelId = req.query.parcelId;
+	let operation = req.query.operation;
+	let sql = 'DELETE FROM db_21912044.Located WHERE Located.parcelId = ? AND Located.operation = ? '
+	let values = [parcelId, operation];
+	let postProcessInsert =   function (err, result) {
+	if (err) throw err;
+		
+
+	}
+	db.query(sql, values, postProcessInsert);
+	let sql2 = 'SELECT parcelId,locId, date, time, operation \
+		FROM Located \
+		WHERE parcelId = ?'
+		values2 = [parcelId]
+		postProcessInsert =   function (err, result2) {
+			if (err) throw err;
+
+			res.json(result2)
+		}
+		db.query(sql2, values2, postProcessInsert);
+
+})
+
+
+app.get('/populateAddNew', function(req,res) {
+	let city_sql = "SELECT locId,city from Locations"
+	let postProcessInsert = function(err,result){
+		if(err) throw err;
+		
+		res.json(result)
+	}
+	db.query(city_sql, postProcessInsert);
+
+
+})
+
+
+
+app.get('/addNewOperation', function(req, res) {
+	let operation = req.query.operation;
+	let parcelId = req.query.parcelId;
+	let date = req.query.date;
+	let city = req.query.city;
+	let locId = req.query.locId;
+
+
+	let sql = "INSERT IGNORE INTO Located (parcelId, locId, operation) VALUES (?,?,?)"
+	//'301', '1', '2022-10-01', 'delivering'
+	let values = [parcelId,locId,date,operation];
+
+	// create a json object containing the inserted location
+	let postProcessInsert =   function (err, result) {
+		if (err) throw err;
+
+		let sql2 = 'SELECT Located.operation, Located.locId, Locations.city FROM Located, Locations WHERE Located.parcelId = ? AND Located.locId = Locations.locID'
+		values2 = [parcelId]
+		let postProcessInsert2 =   function (err, result2) {
+			if (err) throw err;
+
+			res.json(result2)
+		}
+		db.query(sql2, values2, postProcessInsert2);
+
+		
+	};
+
+	db.query(sql, values, postProcessInsert);
+
+	
+// }
+})
 
 // Adding a transfer operation
 
